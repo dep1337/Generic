fd799e98e01d4268d7ef4f3d5f15d1c6ecc29632
dep1337 <127834013+dep1337@users.noreply.github.com>
2023-03-24 10:55:27 +1100

    Initial upload
    Saved Variables file for Generic WoW Addon frame

---
+++ SavedVariables/Generic.lua
@@ -0,0 +1,13 @@
+
+genericText = {
+	"JEN'S BIO BREAK CHECK-IN", -- [1]
+	"1. Knee", -- [2]
+	"2. Bum", -- [3]
+	"3. Stomach", -- [4]
+	"4. Bladder", -- [5]
+	"5. Back", -- [6]
+	"6. Shoulders", -- [7]
+	"7. Chest/Breathing", -- [8]
+	"8. ... and never forget:", -- [9]
+	"Duncan thinks you're perfect!", -- [10]
+}
