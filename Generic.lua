ceff2e3fc524a0c335702e5f3bde66f9b48657de
dep1337 <127834013+dep1337@users.noreply.github.com>
2023-03-24 17:16:39 +1100

    Nightly update
    Recursive research functions (dumpMethods and dumpAttributes) now write sorted output to Saved Variables

--- Generic.lua
+++ Generic.lua
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------------
 --  World of Warcraft addon to ...: 
---	1.	Active time (ie. max(Cast Time, GCD if relevant) / Time in Combat)
+--	1.	Provide a generic framework for a WoW addon
 --	2.	...
 --
 --  (c) March 2023 Duncan Baxter
@@ -9,11 +9,9 @@
 -----------------------------------------------------------------------------
 -- SECTION 1: Constant/Variable definitions
 -----------------------------------------------------------------------------
--- Define some local constants
+-- Define some local "constants"
 local addonName = "Generic"
-local width, height = 256, 384 -- Width and height of parent frame
-local insets = { left = 20,	right = -20, top = -20, bottom = 20 } -- insets.top also needs to include the title container (see Section 2)
-local usableW, usableH = width + insets.right - insets.left, height + insets.top - insets.bottom -- Width and height excluding insets
+local width, height = 250, 400 -- Width and height of parent frame
 
 -- Define some text strings
 local text = {
@@ -36,26 +34,32 @@ local function dumpTable(tbl, lvl) -- Parameters are the table(tbl) and (optiona
 	end
 end
 
--- Debugging function to print the available methods for an object
+-- Research function to print the available methods for an object
 local function dumpMethods(object)
 	local meta = getmetatable(object).__index;
 	for k, v in pairs(meta) do
-		if (type(v) == "function") then print("function ", k, "()") end
+		if (type(v) == "function") then 
+			table.insert(genericMethods, format("function %s()", k))
+		end
 	end
 end
 
--- Debugging function to print the attributes of an object
-local function dumpAttributes(object, lvl)
-	if (type(lvl) ~= "number") then lvl = 1 end -- If no level is provided (ie. omitted or not a number) then set it to level 1
+-- Research function to print the attributes of an object
+local function dumpAttributes(object, p)
+	if (type(p) ~= "string") then p = "." end-- If no prefix is provided (ie. omitted or not a string) then initialise to "."
 	for k, v in pairs(object) do
-		if (type(v) ~= "function") then print(strrep(".", lvl), k, " = ", v) end
-		if (type(v) == "table") then dumpAttributes(v, lvl + 1) end
+		if (type(v) ~= "function") then 
+			table.insert(genericAttributes, format("%s%s = %s", p, k, tostring(v))) -- e.g. ".SomeAttribute = 5"
+		end
+		if (type(v) == "table") then 
+			dumpAttributes(v, format("%s%s.", p, k)) -- Recursively print the attributes of the table
+		end
 	end
 end
 
 -----------------------------------------------------------------------------
 -- SECTION 2: Create the parent frame and implement core functionality including:
--- 1. Properly centred gtitle
+-- 1. Properly centred title
 -- 1. Atlas texture used as the background
 -- 2. 2D player picture used as the portrait (see Section 4)
 -- 3. Exit and Close buttons with "OnClick" handlers (see Section 3)
@@ -73,6 +77,9 @@ end
 local insets = { left = 20,	right = -20, top = -20 - frame.TitleContainer:GetHeight(), bottom = 20 }
 local usableW, usableH = width + insets.right - insets.left, height + insets.top - insets.bottom
 
+-- Set the frame portrait to the player
+frame:SetPortraitToUnit("player")
+
 -- Centre the title relative to the whole width of the frame (not just the title bar, which is off-centre)
 frame:SetTitleOffsets(24, -24) -- The function "SetTitleOffsets (left, right)" only sets the horizontal offsets
 frame:SetTitle(addonName)
@@ -80,10 +87,10 @@ frame:SetTitle(addonName)
 -- Hide the default background (by setting its alpha value to 0) and replace it with an Atlas texture
 frame.Bg:SetAlpha(0)
 frame.texBg = frame:CreateTexture(nil, "BACKGROUND", nil, -8) -- Sub-layers run from -8 to +7: -8 puts our background at the lowest level
-frame.texBg:SetAtlas("ChromieTime-Parchment")
 frame.texBg:SetPoint("TOP", frame.TitleContainer, "BOTTOM")
 frame.texBg:SetPoint("BOTTOMLEFT", frame) -- Need to set BOTTOMLEFT *and* BOTTOMRIGHT because TOP does not provide a "width" reference point
 frame.texBg:SetPoint("BOTTOMRIGHT", frame)
+frame.texBg:SetAtlas("ChromieTime-Parchment")
 
 -- Make the frame draggable
 frame:SetMovable(true)
@@ -138,11 +145,14 @@ frame.btnClose:SetScript("OnClick", function(self, button, down) frame:Hide() en
 -----------------------------------------------------------------------------
 function events:ADDON_LOADED(name)
 	if (name == addonName) then
-		-- Set the frame portrait to the player
-		frame:SetPortraitToUnit("player")
---dumpTable(frame)
---print(dumpTable(GetTextureInfo(frame.Bg)))
---dumpTable(frame.Bg)
+		if (type(genericMethods) == nil) then genericMethods = {} end
+		if (type(genericAttributes) == nil) then genericAttributes = {} end
+
+		dumpMethods(frame)
+		table.sort(genericMethods)
+		dumpAttributes(frame)
+		table.sort(genericAttributes)
+
 		frame:UnregisterEvent("ADDON_LOADED")
 		print(text.txtLoaded)
 	end
